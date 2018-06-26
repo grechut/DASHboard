33d9eeaec4c5a61c6ee4ed73e06072bb54a4376c
jacek <rzeszutekjacek@gmail.com>
2018-06-26 22:47:51 +0200

    Add todo and year slider

--- gapminder/app.py
+++ gapminder/app.py
@@ -1,3 +1,19 @@
+"""
+- year slider FIX MEEE
+- play/pause interval
+- size of bubble => dropdown selection
+- color of bubble => color of continent
+- map of the world => legend for color continents
+
+- multi choice of countires
+- selected countries get label
+- search for countries
+
+- color hue
+
+- make it look better
+"""
+
 import dash
 from dash.dependencies import Output, Event, Input
 import dash_core_components as dcc
@@ -11,20 +27,53 @@ from data import load_data
 data = load_data()
 data_keys = list(data.keys())
 
-
 app = dash.Dash(__name__)
 app.layout = html.Div(
     [
         html.H1("Bubbles"),
+        html.Label("X Axis"),
         dcc.Dropdown(
-            id="x_axis_selection", options=[{"label": k, "value": k} for k in data_keys]
+            id="x_axis_selection",
+            options=[{"label": k, "value": k} for k in data_keys],
+            value=data_keys[0],
         ),
+        html.Label("Y Axis"),
         dcc.Dropdown(
-            id="y_axis_selection", options=[{"label": k, "value": k} for k in data_keys]
+            id="y_axis_selection",
+            options=[{"label": k, "value": k} for k in data_keys],
+            value=data_keys[1],
         ),
+        html.Label("Year"),
+        html.Div([dcc.Slider(id="year_slider")], id="year_slider_container"),
         dcc.Graph(id="main_chart"),
     ]
 )
+
+
+@app.callback(
+    Output("year_slider_container", "children"),
+    [
+        Input(component_id="x_axis_selection", component_property="value"),
+        Input(component_id="y_axis_selection", component_property="value"),
+    ],
+)
+def update_year_slider(x_axis_selection, y_axis_selection):
+    if not x_axis_selection or not y_axis_selection:
+        return []
+
+    min_value = max([data[x_axis_selection].min(), data[y_axis_selection].min()])
+    max_value = min([data[x_axis_selection].max(), data[y_axis_selection].max()])
+
+    return (
+        dcc.Slider(
+            id="year_slider",
+            min=min_value,
+            max=max_value,
+            # step=1,
+            value=max_value,
+            marks={year: str(year) for year in range(min_value, max_value + 1)},
+        ),
+    )
 
 
 @app.callback(
@@ -32,9 +81,13 @@ app.layout = html.Div(
     [
         Input(component_id="x_axis_selection", component_property="value"),
         Input(component_id="y_axis_selection", component_property="value"),
+        Input(component_id="year_slider", component_property="value"),
     ],
 )
-def update_chart(x_axis_selection, y_axis_selection, year=2005):
+def update_chart(x_axis_selection, y_axis_selection, year):
+    if not x_axis_selection or not y_axis_selection or not year:
+        return
+
     year = str(year)
 
     return {
@@ -45,10 +98,10 @@ def update_chart(x_axis_selection, y_axis_selection, year=2005):
                 mode="markers+text",
                 text=data[x_axis_selection][year].index,
             )
-        ]
+        ],
+        "layout": {"title": year},
     }
 
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-
