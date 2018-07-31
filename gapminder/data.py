f73dc24d94194a6ba481f2bf2baa0a9557ed1e2b
jacek <rzeszutekjacek@gmail.com>
2018-07-31 22:03:17 +0200

    Fix loading of data and start data chapter

--- gapminder/data.py
+++ gapminder/data.py
@@ -3,8 +3,10 @@ import os
 import pandas as pd
 
 
+MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(MODULE_DIR, "data")
+
 TOTAL_POPULATION = "Total population"
-DATA_FMT = "data/{}"
 COLOR_MAPPING = {
     "europe": "#ffff66",
     "africa": "#66ffff",
@@ -13,8 +15,8 @@ COLOR_MAPPING = {
 }
 
 
-def load_df(file_name):
-    df = pd.read_excel(DATA_FMT.format(file_name))
+def load_df(file_path):
+    df = pd.read_excel(file_path)
     df = df.set_index(df.columns[0])
 
     # Some files got strings, some ints, why?
@@ -28,11 +30,11 @@ def load_df(file_name):
 def load_data():
     data = {}
 
-    for file_name in os.listdir("data"):
+    for file_name in os.listdir(DATA_DIR):
         if ".xlsx" not in file_name:
             continue
 
-        df = load_df(file_name)
+        df = load_df(os.path.join(DATA_DIR, file_name))
         data[df.index.name] = df
 
     return data
@@ -72,5 +74,5 @@ def get_config(data, x_axis, y_axis, z_axis=TOTAL_POPULATION, supported_countrie
 
 
 def get_countires_mapping():
-    df = pd.read_csv(DATA_FMT.format("country_mapping.csv"))
+    df = pd.read_csv(os.path.join(DATA_DIR, "country_mapping.csv"))
     return dict(zip(df["name"], df["Gapminder's 4 world regions"].map(COLOR_MAPPING)))
