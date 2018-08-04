9ee3db8c057d817c8d97976a88777e5e3c61fc51
jacek <rzeszutekjacek@gmail.com>
2018-08-04 22:08:16 +0200

    Do plotly chapter

--- exercise_solutions.py
+++ exercise_solutions.py
@@ -1,11 +1,38 @@
-# Data #1
+# Data for dashboard #1
 
 data["Total population"]["2000"]
 data["Total population"]["2000"].loc["Poland"]
 # or
 data["Total population"].loc["Poland", "2000"]
 
-# Data #2
+# Data for dashboard #2
 
-new_conf = get_config(data, "Life expectancy", "Murder per 100,000, age adjusted")
-new_conf["x_df"].loc["Japan", "2000"]
+life_murder_conf = get_config(
+    data, "Life expectancy", "Murder per 100,000, age adjusted"
+)
+life_murder_conf["x_df"].loc["Japan", "2000"]
+
+# First graphs
+
+X_AXIS = "Life expectancy"
+Y_AXIS = "Murder per 100,000, age adjusted"
+life_murder_conf = get_config(data, X_AXIS, Y_AXIS)
+year = life_murder_conf["years"][-1]
+
+plotly.offline.iplot(
+    {
+        "data": [
+            go.Scatter(
+                x=life_murder_conf["x_df"][year],
+                y=life_murder_conf["y_df"][year],
+                mode="markers",
+                marker={"size": 10},
+            )
+        ],
+        "layout": {
+            "title": f"Year {year}",
+            "xaxis": {"title": X_AXIS},
+            "yaxis": {"title": Y_AXIS},
+        },
+    }
+)
